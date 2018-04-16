09eeb331df1a6586e4315626ff17b699cda766a7
dimaeromin97 <dimaeromin97@gmail.com>
2018-04-16 18:04:05 +0300

    v1.0.0

--- test/prezent.js
+++ test/prezent.js
@@ -1,7 +1,11 @@
 window.addEventListener("load", function() {
 
 	var smoothStickyMethod = new smoothSticky(".column_sticky", {
-		offsetTopElement: document.querySelector("#header")
+		offsetTopElement: document.querySelector("#header"),
+		indent: {
+			top: 30,
+			bottom: 30
+		}
 	});
 
 	return;
